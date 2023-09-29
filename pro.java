f98354e9bffeafac3b02748289ad5babe56a06ee
Gopalakrishnan a <126150922+bsgopal@users.noreply.github.com>
2023-09-29 22:12:40 +0530

    Add files via upload
    i have creating veghicles  details using java

---
+++ pro.java
@@ -0,0 +1,10 @@
+package gopal.gk.projects.me;
+
+public class pro {
+	 final static String collegename = "LOYOLA";
+	
+	
+	public static void main(String[] args) {
+		System.out.println(Math.pow(5,3));
+	}
+}
